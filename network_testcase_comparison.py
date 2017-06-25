1cf6a51f24c4542982318e39452c79d1b0ce6d34
shanshanhsieh <hsieh@arch.ethz.ch>
2017-06-25 15:47:06 +0800

    add DC data from cea

--- network_testcase_comparison.py
+++ network_testcase_comparison.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 network = 'network3'
 network_type = 'DH'
 
-path_to_matlab = os.path.abspath(r'C:\Users\fcl2\Documents\MATLAB\network\network3_DH\0-1000')
+path_to_matlab = os.path.abspath(r'C:\Users\fcl2\Documents\MATLAB\network\network3_DH')
 simulink_network_qloss = pd.read_csv(os.path.join(path_to_matlab,'%s_qloss_supply.csv' %network))
 simulink_network_dP = pd.read_csv(os.path.join(path_to_matlab, '%s_dP_supply.csv' %network), header= None)
 path_to_cea = os.path.abspath(r'C:\reference-case-open\baseline\outputs\data\optimization\network\layout')
@@ -21,42 +21,58 @@ cea_network_qloss = pd.read_csv(os.path.join(cea_network, '%s_qloss_Supply.csv' 
 cea_network_dP = pd.read_csv(os.path.join(cea_network, '%s_P_DeltaP.csv' %network_type))['pressure_loss_supply_Pa']
 
 delta_qloss = cea_network_qloss.sub(simulink_network_qloss)
-percent_d_qloss = delta_qloss[0:1000].div(simulink_network_qloss[0:1000])
+percent_d_qloss = delta_qloss[0:8760].div(simulink_network_qloss[0:8760])
 
 # delta_ploss = cea_network_dP.sub(simulink_network_dP, axis=0)
 delta_ploss = simulink_network_dP.sub(cea_network_dP, axis=0)
-percent_d_ploss = delta_ploss[0:1000].div(simulink_network_dP[0:1000])
+percent_d_ploss = delta_ploss[0:8760].div(simulink_network_dP[0:8760])
 
 
 
 # plotting
-x = np.arange(1000)
+x = np.arange(8760)
 #dashes = [10, 5, 100, 5]  # 10 points on, 5 off, 100 on, 5 off
 
-fig1, (ax1,ax2) = plt.subplots(2, sharex=True)
-line1, = ax1.plot(x, cea_network_dP[0:1000], label='cea')
-line2, = ax1.plot(x, simulink_network_dP[0:1000], label='simulink')
+fig_ploss, (ax1, ax2) = plt.subplots(2, sharex=True)
+line1, = ax1.plot(x, cea_network_dP[0:8760], label='cea')
+line2, = ax1.plot(x, simulink_network_dP[0:8760], label='simulink')
 ax1.legend(loc='lower right')
 
-line3, = ax2.plot(x, percent_d_ploss[0:1000], label='d_ploss')
+line3, = ax2.plot(x, percent_d_ploss[0:8760], label='d_ploss')
 ax2.legend(loc='lower right')
+
+fig_ploss.savefig('%s_ploss' %network)
+
+
+axs = {}
+figs_qloss = plt.figure(figsize=(30,20))
+figs_qloss.subplots_adjust(hspace=1)
+for edge in range(simulink_network_qloss.shape[1]):
+        plot_number = 811 + edge
+        axs[edge]=figs_qloss.add_subplot(plot_number)
+        column = simulink_network_qloss.columns[edge]
+        line3, = axs[edge].plot(x, cea_network_qloss[0:8760][column])
+        line4, = axs[edge].plot(x, simulink_network_qloss[0:8760][column])
+        #axs[edge].legend(loc='lower right')
+        axs[edge].set_title(column)
+
+figs_qloss.savefig('%s_qloss' %network)
+
+axs_1 = {}
+figs_qloss_1 = plt.figure(figsize=(30,20))
+figs_qloss_1.subplots_adjust(hspace=1)
+for edge in range(simulink_network_qloss.shape[1]):
+        plot_number = 811 + edge
+        axs_1[edge]=figs_qloss_1.add_subplot(plot_number)
+        column = simulink_network_qloss.columns[edge]
+        line5, = axs_1[edge].plot(x, percent_d_qloss[0:8760][column])
+        #axs[edge].legend(loc='lower right')
+        axs_1[edge].set_title(column)
+
+figs_qloss_1.savefig('%s_qloss_percentage' %network)
 
 
 
 # plt.show()
-axs = {}
-figs = plt.figure(figsize=(30,20))
-figs.subplots_adjust(hspace=1)
-for edge in range(simulink_network_qloss.shape[1]):
-        plot_number = 811 + edge
-        axs[edge]=figs.add_subplot(plot_number)
-        column = simulink_network_qloss.columns[edge]
-        line3, = axs[edge].plot(x, cea_network_qloss[0:1000][column])
-        line4, = axs[edge].plot(x, simulink_network_qloss[0:1000][column])
-        #axs[edge].legend(loc='lower right')
-        axs[edge].set_title(column)
-
-plt.show()
-
 print percent_d_ploss.describe()
 print percent_d_qloss.describe()
